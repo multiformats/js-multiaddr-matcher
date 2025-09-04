72ea7d1e1f16ba7fba294cf939345aa4cb355248
achingbrain <alex@achingbrain.net>
2025-09-04 13:33:18 +0300

    chore: linting

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -234,7 +234,7 @@ describe('multiaddr matcher', () => {
     '/dnsaddr/ipfs.io/ws',
     '/ip4/1.2.3.4/tcp/3456/http/p2p-webrtc-star',
     '/ip4/0.0.0.0/tcp/12345/ipfs/QmUjNmr8TgJCn1Ao7DvMy4cjoZU15b9bwSCBLE3vwXiwgj/p2p-circuit/webrtc',
-    '/ip4/0.0.0.0/tcp/12345/ipfs/QmUjNmr8TgJCn1Ao7DvMy4cjoZU15b9bwSCBLE3vwXiwgj/p2p-circuit/webrtc/p2p/QmTysQQiTGMdfRsDQp516oZ9bR3FiSCDnicUnqny2q1d79',
+    '/ip4/0.0.0.0/tcp/12345/ipfs/QmUjNmr8TgJCn1Ao7DvMy4cjoZU15b9bwSCBLE3vwXiwgj/p2p-circuit/webrtc/p2p/QmTysQQiTGMdfRsDQp516oZ9bR3FiSCDnicUnqny2q1d79'
   ]
 
   const goodIPFS = [
